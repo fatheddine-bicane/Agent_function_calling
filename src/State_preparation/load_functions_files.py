30ff95272b68a0ac53f34ad3f85fa05cbc242267
fatheddine-bicane <fatheddine04@gmail.com>
2026-04-10 21:49:28 +0100

    feat: load and validate JSON files and return a list of dict with the valid function definitions

---
+++ src/State_preparation/load_functions_files.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import itertools
+import json
+from .schema_validation import validateFunctionsDefinitions
+from Exceptions.StatePreparationExceptions import MultipleFunctionDefinitionException
+from jsonschema import ValidationError
+
+
+def load_functions() -> list[dict]:
+    folder_path = Path("src/functions_deffinition")
+
+    lowercase_json = folder_path.glob("*.json")
+    uppercase_json = folder_path.glob("*.JSON")
+    all_json_files = itertools.chain(lowercase_json, uppercase_json)
+
+    functions_definitions = []
+    for json_file in all_json_files:
+        with json_file.open() as function_definition_file:
+            try:
+                function_definition = json.load(function_definition_file)
+                validateFunctionsDefinitions(function_definition)
+                functions_definitions.append(function_definition)
+            except json.JSONDecodeError as e:
+                print(f"Error: file \"{json_file}\"\n    JSON syntax: {e.msg}.")
+                print("    Warning: Function not loaded!")
+            except ValidationError as e:
+                print(f"Error: file \"{json_file}\"\n    Schema : {e.message}.")
+                print("    Warning: Function not loaded!")
+            except MultipleFunctionDefinitionException as e:
+                print(f"Error: file \"{json_file}\"\n    Multiple function definitions : {e.message}.")
+                print("    Warning: Functions not loaded!")
+
+    return functions_definitions
