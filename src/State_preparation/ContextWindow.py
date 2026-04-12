2f814e5933487042ec01bdeda7519fcab905079b
fatheddine-bicane <fatheddine04@gmail.com>
2026-04-12 17:36:29 +0100

    feat: add appendMessage() function to append and manage context window size

--- src/State_preparation/ContextWindow.py
+++ src/State_preparation/ContextWindow.py
@@ -3,3 +3,17 @@ class ContextWindow:
         self._messages_lists: list[dict[str, str]] = []
         self._index = 0
         self._messages_limit = messages_limit
+
+
+    def appendMessage(self, role: str, content: str) -> None:
+        if (len(self._messages_lists) < self._messages_limit):
+            self._messages_lists.append({
+                "role": role, "content": content
+            })
+
+        else:
+            if self._index == self._messages_limit:
+                self._index = 0
+
+            self._messages_lists[self._index] = {"role": role, "content": content}
+            self._index += 1
