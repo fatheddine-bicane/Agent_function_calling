4e377ef8b58eba1b1acb16f57587ed2b7f990437
fatheddine-bicane <fatheddine04@gmail.com>
2026-04-12 17:40:24 +0100

    refactor: tokenize messages list and apply chat template handled by ContextWindow class

--- src/State_preparation/ContextWindow.py
+++ src/State_preparation/ContextWindow.py
@@ -1,3 +1,8 @@
+from llm_sdk import Small_LLM_Model
+from .load_functions_files import load_functions
+from typing import cast, Any, Callable
+from torch import Tensor
+
 class ContextWindow:
     def __init__(self, *, messages_limit: int=50) -> None:
         self._messages_lists: list[dict[str, str]] = []
@@ -17,3 +22,16 @@ class ContextWindow:
 
             self._messages_lists[self._index] = {"role": role, "content": content}
             self._index += 1
+
+
+    def tokenizeContextWindow(self, llm: Small_LLM_Model) -> Tensor:
+        functions: list[dict] = load_functions()
+
+        initial_prompt = llm._tokenizer.apply_chat_template(
+            conversation=self._messages_lists,
+            tools=cast(list[dict[str, Any] | Callable[..., Any]], functions),
+            add_generation_prompt=True,
+            tokenize=False
+        )
+
+        return llm.encode(cast(str, initial_prompt))
