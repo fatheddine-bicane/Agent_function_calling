aa649777f9b741d1091fa15e2d106cd02fefcfc8
fatheddine-bicane <fatheddine04@gmail.com>
2026-04-25 18:31:53 +0100

    feat: build a token level state machine using the model tokens vocabulary

--- src/Constrained_decoding/FiniteStateMachine.py
+++ src/Constrained_decoding/FiniteStateMachine.py
@@ -11,6 +11,7 @@ class FiniteStateMachine:
             regex: str = self.__schemaToRegex(schema)
             parsed_regex: Pattern = parse_pattern(regex)
             self.fsm: FSM = parsed_regex.to_fsm()
+            self.token_fsm_index: dict = self.__tokenFsmIndex(llm, self.fsm)
 
         return
 
@@ -57,3 +58,35 @@ class FiniteStateMachine:
             return r'\[(' + items_regex + r'(, ?' + items_regex + r')*)?\]'
 
         raise ValueError(f"Unsupported schema type: {schema}")
+
+
+    def __tokenFsmIndex(self, llm: Small_LLM_Model, fsm: FSM) -> dict:
+        token_fsm_index: dict[State, dict[int, State]] = {}
+
+        tokens_vocabulary = llm._tokenizer.get_vocab()
+
+        # Local variable lookups are much faster inside tight loops
+        fsm_map = fsm.map
+        symbol_mapping = fsm.alphabet._symbol_mapping
+
+        for state in fsm.states:
+            token_fsm_index[state] = {}
+
+            for token_string, token_id in tokens_vocabulary.items():
+                current_state = state
+                is_valid_token = True
+
+                for char in token_string:
+                    fsm_symbol_id = symbol_mapping.get(char, anything_else)
+                    state_transitions = fsm_map.get(current_state, {})
+
+                    if fsm_symbol_id not in state_transitions:
+                        is_valid_token = False
+                        break
+
+                    current_state = state_transitions[fsm_symbol_id] #type: ignore
+
+                if is_valid_token:
+                    token_fsm_index[state][token_id] = current_state
+
+        return token_fsm_index
